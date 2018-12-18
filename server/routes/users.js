3a90ecd19d28a9c73d027b7f6019a3b09d3f106b
Jon Rosado <jon.rosado42@gmail.com>
2018-12-18 13:37:17 -0500

    Connect to firebase realtime db. Set will overwrite data

---
+++ server/routes/users.js
@@ -0,0 +1,38 @@
+// const firebase = require("firebase");
+var express = require("express");
+var router = express.Router();
+const admin = require('firebase-admin');
+const serviceAccount = require('../../serviceAccountKey.json');
+const firebaseURL = process.env.FIREBASE_DB_URL;
+
+admin.initializeApp(({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dog-ear-d.firebaseio.com/"
+}))
+
+const database = admin.database();
+const ref = database.ref('dog-ear-d')
+
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("You've reached the homepage");
+});
+
+// router.get("/users", (req, res, next) => {
+
+// })
+
+router.post("/user", (req, res, next) => {
+  console.log(req.body)
+  const newUser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  }
+  const usersRef = ref.child('users');
+  usersRef.set({newUser})
+  res.json(newUser);
+});
+
+module.exports = router;
